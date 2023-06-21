34150d57b6665af52b0a42e1b3f089bab94c8571
EmilioC <emicorgul@gmail.com>
2023-06-21 21:50:56 +0200

    Update

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from pydantic import BaseModel
 from fastapi import FastAPI
 from fastapi import Body, Query, Path
 
+# Activar entorno virtual .\venv\Scripts\activate
+# Lanzar aplicación: uvicorn main:app --reload
+
 app = FastAPI() 
 
 # Models
@@ -36,8 +39,18 @@ def create_person( person: Person = Body(...)):
 @app.get("/person/detail")
 def show_person( 
                 #User no podrá enviar nunca menos de 1 y máximo 50 caracteres
-        name: Optional[str] = Query (None, min_length=1, max_length=50),
-        age: str = Query(...),
+        name: Optional[str] = Query (
+            None, 
+            min_length=1, 
+            max_length=50,
+            title= "Person Name",
+            description="This is the person name. It's between 1 and 50 characters"
+            ),
+        age: str = Query(
+            ...,
+            title="Person Age",
+            description="This is the person age. It's required"
+            ),
         
 ):
     return {name: age}
@@ -46,6 +59,25 @@ def show_person(
 
 @app.get("/person/detail/{person_id}")
 def show_person(
-    person_id: int = Path(...,gt=0)
+    person_id: int = Path(
+        ...,
+        gt=0,
+        title="Person Id",
+        description="This is the person id. It's required"
+        )
 ):
     return {person_id: "It exists¡"}
+
+# Validaciones: Request Body
+
+@app.put("/person/{person_id}")
+def update_person(
+    person_id: int = Path (
+        ..., 
+        title="Person ID",
+        description="This is the person ID",
+        gt=0
+    ),
+        person: Person = Body (...)
+):
+    return person
